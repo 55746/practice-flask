32967b91e0320c541ed91610e2af29bdbcb6498e
55746 <Lawhead.gavin@live.jobcorps.org>
2022-05-01 18:33:40 +0000

    almost done

--- src/app.py
+++ src/app.py
@@ -12,14 +12,15 @@ def hello_world():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
+    todos.append(request_body)
     print("Incoming request with the following body", request_body)
     return jsonify(todos)
 
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    print("This is the position to delete: ", position)
     todos.pop(position)
+    print("This is the position to delete: ", position)
     return jsonify(todos)
 
 
